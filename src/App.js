55618ddfaa6fbcb53a9e0726c54e685e405ede9d
Rebecca Stone <you@example.com>
2022-02-24 21:01:28 -0600

    wrote an outline of tasks needed

--- src/App.js
+++ src/App.js
@@ -9,3 +9,26 @@ const App = () => {
   );
 };
 export default App;
+
+//I need to build three pages
+
+// home '/'
+//needs a button with id of "order-pizza"
+
+//form '/pizza'
+// Data should look something like
+//     name: string, text input, id of "name-input", key string
+//     size: string, dropdown, id of "size-dropdown", key string
+//     topping1: bool, checkbox, name each separately!, key bool
+//     topping2: bool, checkbox, name each separately!, key bool
+//     topping3: bool, checkbox, name each separately!, key bool
+//     topping4: bool, checkbox, name each separately!, key bool
+//     special: string, text input id of "special-text", key string
+//     submit: button, id of "order-button", 
+
+//     error message is "name must be at least 2 characters"
+
+//order placed 
+
+
+//Then start cypress tests
